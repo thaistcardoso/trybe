adb9952fb77fa1ec0f0fcaa2e50a3ecb326e1aa1
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-06 20:55:32 -0300

    add pasta bloco 4 de Introdução do JS

---
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
@@ -0,0 +1,12 @@
+const a = 7;
+const b = 5;
+
+console.log(`resultado da soma de ${a} + ${b} = ${a+b}`);
+console.log(`resultado da soma de ${a} - ${b} = ${a-b}`);
+console.log(`resultado da soma de ${a} * ${b} = ${a*b}`);
+console.log(`resultado da soma de ${a} / ${b} = ${a/b}`);
+console.log(`resultado da soma de ${a} % ${b} = ${a%b}`);
+
+
+
+
