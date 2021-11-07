3738aa0c7628f0af4042cabb5ea9dc765fbabaea
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-06 21:07:55 -0300

    Add exercicios 2 e 3

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
@@ -1,12 +1,30 @@
-const a = 7;
-const b = 5;
+ const a = 8;
+ const b = 57;
+ const c = 7;
 
-console.log(`resultado da soma de ${a} + ${b} = ${a+b}`);
-console.log(`resultado da soma de ${a} - ${b} = ${a-b}`);
-console.log(`resultado da soma de ${a} * ${b} = ${a*b}`);
-console.log(`resultado da soma de ${a} / ${b} = ${a/b}`);
-console.log(`resultado da soma de ${a} % ${b} = ${a%b}`);
+// exercicio 1 
+// console.log(`resultado da soma de ${a} + ${b} = ${a+b}`);
+// console.log(`resultado da soma de ${a} - ${b} = ${a-b}`);
+// console.log(`resultado da soma de ${a} * ${b} = ${a*b}`);
+// console.log(`resultado da soma de ${a} / ${b} = ${a/b}`);
+// console.log(`resultado da soma de ${a} % ${b} = ${a%b}`);
 
+// exercicio 2
+// if (a > b ){
+//     console.log(a, "maior que ", b);
+// }else {
+//     console.log(b, "maior que ", a);
+// };
+
+// exercicio 3
+// if ( a > b && a > c){
+//     console.log(a, "maior que ", b, "e ", c );
+// }else if ( b > a && b > c) {
+//     console.log(b, "maior que ", a, "e ", c);
+// }else{
+//     console.log(c, "maior que ", a, "e ", b);
+
+// }
 
 
 
