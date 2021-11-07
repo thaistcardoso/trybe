8d4dc7bba879bc1e6abe330ea61986ede6747c76
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-07 00:59:46 -0300

    add exercicio 5

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
@@ -1,4 +1,4 @@
-const a = 0;
+// const a = 0;
 //  const b = -57;
 //  const c = 7;
 
@@ -26,8 +26,6 @@ const a = 0;
 // }
 
 
-
-
 // exercicio 4
 // if (a > 0){
 //     console.log("o número ",a, "é positive");
@@ -37,6 +35,27 @@ const a = 0;
 //     console.log("o número ",a, "é zero");
 // }
 
+// 5.Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo
+
+const a = 80;
+const b = 300;
+const c = 100;
+
+// let angulos = a + b + c;
+// let angulosOk = a > 0 &&  b > 0 &&  c > 0;
+
+
+    if (a > 0 && b > 0 && c > 0) {
+        if (a + b + c === 180 ){
+            console.log(true);
+        }else if (a + b + c !== 180){
+            console.log(false);
+        };
+    }else {
+    console.log("Agulo informado inválo, por favor informe um angulo com valor positivo")
+    };
 
 
