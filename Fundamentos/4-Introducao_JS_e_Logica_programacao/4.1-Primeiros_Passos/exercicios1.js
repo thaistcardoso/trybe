52c32a7cab8316420e6ba7b988de1d679e30a7f5
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-06 22:15:58 -0300

    Add exercicios 4

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicios1.js
@@ -1,6 +1,6 @@
- const a = 8;
- const b = 57;
- const c = 7;
+const a = 0;
+//  const b = -57;
+//  const c = 7;
 
 // exercicio 1 
 // console.log(`resultado da soma de ${a} + ${b} = ${a+b}`);
@@ -23,8 +23,20 @@
 //     console.log(b, "maior que ", a, "e ", c);
 // }else{
 //     console.log(c, "maior que ", a, "e ", b);
-
 // }
+
+
+
+
+// exercicio 4
+// if (a > 0){
+//     console.log("o número ",a, "é positive");
+// }else if(a<0){
+//     console.log("o número ",a, "é negative");
+// }else{
+//     console.log("o número ",a, "é zero");
+// }
+
 
 
 
