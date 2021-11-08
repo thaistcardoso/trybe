602c573c3844cff2d75c29fb9d4073d3d670b422
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-08 01:37:15 -0300

    add exercicio 11 iniciado

---
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicio11.js
@@ -0,0 +1,14 @@
+const salBruto = 3000;
+
+
+
+if (salBruto < 1556,94) {
+  let salLiq = salBruto + salBruto * 0.08;
+
+        
+
+    case (salBruto < 1903,98):
+        salLiq;
+        break;
+    case
+}
