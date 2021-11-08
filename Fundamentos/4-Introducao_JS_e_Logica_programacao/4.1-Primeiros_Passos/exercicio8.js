f04971a64040c8345883c29499a0579a85e83192
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-08 00:47:31 -0300

    add exercicios 8 a 9

---
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.1-Primeiros_Passos/exercicio8.js
@@ -0,0 +1,16 @@
+const num1 = 7;
+const num2 = 20;
+const num3 = 55;
+
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0 ){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+let isEven = false
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0 ){
+    isEven  = true;
+};
+console.log(isEven);
