129ccf46ba5a930984d837e7f16405778300a18d
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-08 19:24:47 -0300

    exercicios 1 a 2

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.2-array_loop_for/Exercicio1.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.2-array_loop_for/Exercicio1.js
@@ -1,4 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let i = 0; i < numbers.length; i += 1){
-    console.log(numbers[i]);
+
+// exercício1
+// for (let i = 0; i < numbers.length; i += 1){
+//     console.log(numbers[i]);
+// }
+
+//exercicio2
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i += 1){
+//     soma += numbers[i]; // soma = soma + numbers[i]; 
+//     console.log(soma);
+// }
+
+// exercicio3
+
+let soma = 0
+for (let i = 0; i < numbers.length; i += 0 ){
+    soma += numbers[i];
 }
+let mediaArit = soma / numbers.length;
+console.log(mediaArit);
