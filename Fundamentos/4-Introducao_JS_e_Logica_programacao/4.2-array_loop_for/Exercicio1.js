bd97b9747799c44e9ed0895753dc7681fa9dafa4
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-08 21:17:17 -0300

    exercicios 3 a 8

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.2-array_loop_for/Exercicio1.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.2-array_loop_for/Exercicio1.js
@@ -14,9 +14,53 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio3
 
-let soma = 0
-for (let i = 0; i < numbers.length; i += 0 ){
-    soma += numbers[i];
+// let soma = 0
+// for (let i = 0; i < numbers.length; i += 1 ){
+//     soma += numbers[i];
+// }
+// let mediaArit = soma / numbers.length;
+// console.log(mediaArit);
+
+// exercicio4
+// if ( mediaArit > 20){
+//     console.log("Valor maior que 20");
+// }else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+// exercicio5
+// let maior = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1){
+//     if (numbers[i] > maior) {
+//         maior = numbers[i];
+//     }
+// }
+// console.log(maior);
+
+// exercicio 6
+// let impares = numbers[0];
+
+
+// for (let i =0; i < numbers.length; i += 1){
+//     if ()
+// }
+
+//exercicio 7
+// let menor = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1){
+//     if (numbers[i] < menor) {
+//         menor = numbers[i];
+//     }
+// }
+// console.log(menor);
+
+let array = [];
+
+for (i = 1; i < 26; i += 1){
+    array.push(i);
 }
-let mediaArit = soma / numbers.length;
-console.log(mediaArit);
+for (i = 0; i <array.length; i += 1){
+    console.log(array[i]);
+}
