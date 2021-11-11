63a1f7d8c2074237bb3d972dd34d7bf9e9980261
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-10 23:25:48 -0300

    tentativa exercicio 4

--- Fundamentos/4-Introducao_JS_e_Logica_programacao/4.3-Logica_programacao_algoritmos/exercicio4.js
+++ Fundamentos/4-Introducao_JS_e_Logica_programacao/4.3-Logica_programacao_algoritmos/exercicio4.js
@@ -5,3 +5,22 @@
 //   *
 //  ***
 // *****
+
+let n =5;
+let asterisco = '*';
+let line = '';
+posicao = n;
+
+for (let i = 0; i < n; i += 1){
+    for(let coluna = 0; coluna < n; coluna += 1){
+        if(coluna < posicao){
+            line = line + ' ';
+        }else{
+            line = line + asterisco;
+        }
+
+    }
+    console.log(line)
+    line = '';
+    posicao -= 2;
+}
