c289af6b259bd0aabbb64bb87970a2ef83f211bb
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-11-22 02:41:26 -0300

    exercicio Calendario 1, 2 e 3(parte)

---
+++ Fundamentos/5-DOM_Eventos_Web-Storage/5.3-Eventos/script.js
@@ -0,0 +1,77 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+//   function addFridayAndHoliday() {
+//         const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+//         const monthDayList = document.querySelector('#days');
+
+//         for (let numero = 0; numero < dezDaysList.length; numero += 1) {
+//             const day = dezDaysList[numero];
+//             if (day === 24 || day === 25 || day === 31) {
+//                 monthDayList[numero].classList.add('holiday');
+//             };
+//             if (day === 4 || day === 11 || day === 18 || day === 25) {
+//                 monthDayList[numero].classList.add('friday');
+//             };
+//             console.log(day, typeof(day));
+//         };
+//     };
+//     addFridayAndHoliday();
+    
+
+    function createDaysOfMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const monthDays = document.getElementById('days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const numDays = dezDaysList[index];
+        const monthDayListItem = document.createElement('li');
+        if (numDays === 24 || numDays === 31) {
+            monthDayListItem.innerHTML = numDays;
+            monthDayListItem.className = 'day holyday';
+            monthDays.appendChild(monthDayListItem); 
+        } else if (numDays === 4 || numDays === 11 ||numDays === 18) {
+            monthDayListItem.innerHTML = numDays;
+            monthDayListItem.className = 'day friday';
+            monthDays.appendChild(monthDayListItem); 
+        } else if (numDays === 25) {
+            monthDayListItem.innerHTML = numDays;
+            monthDayListItem.className = 'day holiday friday';
+            monthDays.appendChild(monthDayListItem); 
+        } else {
+            monthDayListItem.innerHTML = numDays;
+            monthDayListItem.className = 'day';
+            monthDays.appendChild(monthDayListItem); 
+        };
+             
+    };
+};
+createDaysOfMonth();
+
+function addButtonHoliday(string){
+    const divBotao = document.querySelector('.buttons-container');
+    const holidayBtn = document.createElement('button');
+    holidayBtn.id = 'btn-holiday';
+    divBotao.appendChild(holidayBtn);
+    holidayBtn.innerHTML = string;
+};
+
+addButtonHoliday('Feriados');
+
+function holidayColor() {
+    const holidayBtn = document.getElementById('btn-holiday');
+    const holidays = document.querySelector('.holiday')
+}
