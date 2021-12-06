849493e3e3d89154b4d545399aafb344fd8331cd
Thais Cardoso <thaistauane.cardoso@gmail.com>
2021-12-06 20:32:02 -0300

    exemplos aula e exercicio 1

---
+++ Fundamentos/7-Introducao_Javascript_ES6_Teste_Unitarios/7.2-JavaScript_ES6-Objects/exemplos_aula.js
@@ -0,0 +1,86 @@
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+// const coolestTvShow = {
+//   name: "BoJack Horseman",
+//   genre: "adult animation",
+//   createdBy: "Raphael Bob-Waksberg",
+//   favoriteCharacter: "Princess Carolyn",
+//   quote: "Princess Carolyn always lands on her feet.",
+//   seasons: 6,
+// };
+
+// // for (const property in coolestTvShow) {
+// //   console.log(property);
+// // }
+
+// console.log(Object.keys(coolestTvShow));
+
+// // [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+// const student = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkill: 'Ótimo',
+// };
+
+// const listSkillsValuesWithFor = (student) => {
+//   const skills = [];
+//   for(skill in student) {
+//     skills.push(student[skill]);
+//   }
+
+//   return skills;
+// };
+
+// const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// // Sem Object.values
+// console.log(listSkillsValuesWithFor(student));
+
+// // Com Object.values
+// console.log(listSkillsValuesWithValues(student));
+
+// const países = {
+//   França: 'Paris',
+//   Brasil: 'Brasília',
+//   Espanha: 'Madrid',
+//   Portugal: 'Lisboa',
+// };
+// const pairKeyValue = Object.entries(países);
+// console.log(pairKeyValue);
+
+// for(index in pairKeyValue) {
+//   console.log('--------');
+//   console.log('País:', pairKeyValue[index][0]);
+//   console.log('Capital:', pairKeyValue[index][1]);
+// };
+
+// const starships = {
+//   option1: "Tardis",
+//   option2: "Millennium Falcon",
+//   option3: "Enterprise",
+//   option4: "Heart of Gold"
+// }
+
+
+// console.log(Object.values(starships))
+
+const top3Albums = {
+ 1: 'Nightfall in Middle-Earth',
+ 2: 'Imaginations from the Other Side',
+ 3: 'Somewhere Far Beyond',
+  };
+ 
+  console.log(Object.entries(top3Albums))
